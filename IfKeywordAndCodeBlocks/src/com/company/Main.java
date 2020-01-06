950d0a8374b7054d1cf4a117f8f94f7a977329d4
Lorena <lorena.tacal@gmail.com>
2020-01-06 22:17:43 +0000

    Methods

---
+++ IfKeywordAndCodeBlocks/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            calculateScore(true, 800, 5, 100);
+            calculateScore(true, 10000, 8, 200);
+
+        }
+
+        public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+          if(gameOver) {
+            //scope = variables inside the code block. This can't be access outside of the code block
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore +=2000;
+            System.out.println("Your final score was " + finalScore);
+          }
+        }
+}
