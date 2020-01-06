fb49d42bd365291b41159834a97a5bbf76412edd
Lorena <lorena.tacal@gmail.com>
2020-01-06 10:00:38 +0000

    Learn expressions

--- KeywordsAndExpressions/src/com/company/Main.java
+++ KeywordsAndExpressions/src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    double kilometers = (100 * 1.609344);
+	    int highScore = 50; // expression highScore = 50
+
+	    if (highScore == 50) { //highScore == 50 -> expression
+	        System.out.println("This is an expression"); //expression "This is an expression"
+        }
     }
 }
