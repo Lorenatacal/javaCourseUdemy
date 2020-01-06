6ee40010afa70e4a2df310b270407b5ec0080a38
Lorena <lorena.tacal@gmail.com>
2020-01-06 10:13:03 +0000

    Statments, white space and indenting

---
+++ StatmentsWhiteSpaceAndIndenting/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 50; //the complete line is a statement
+        if (myVariable == 50) {
+            System.out.println("Indented");
+        }
+
+        myVariable++; //a statement
+        System.out.println("This is a test"); //the complete line is a statement
+        System.out.println("This is" +
+                " another" +
+                " still more.");
+        //this is a statement
+        //white space is the space between  expressions, operators and so on
+    }
+}
