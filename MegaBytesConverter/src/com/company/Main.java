9751be25f5e1d1ae10c3334d46ec77c3e36f6ba8
Lorena <lorena.tacal@gmail.com>
2020-01-07 22:34:16 +0000

    Add MegaBytesConverter

---
+++ MegaBytesConverter/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import static com.company.MegaBytesConverter.printMegaBytesAndKiloBytes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(5300);
+    }
+}
