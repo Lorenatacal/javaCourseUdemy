4f9990cac6f338c58770ab0fcdb1dbb74236c3f9
Lorena <lorena.tacal@gmail.com>
2020-01-08 12:01:10 +0000

    Add EqualSumChecker function()

---
+++ EqualSumChecker/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import static com.company.EqualSumChecker.hasEqualSum;
+
+public class Main {
+
+    public static void main(String[] args) {
+      System.out.println(hasEqualSum(2, 3, 5));
+      System.out.println(hasEqualSum(2, 3, 6));
+    }
+}
