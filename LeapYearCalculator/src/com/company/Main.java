9aabd121f1195a3e50aa89faba6a21b5cd8a8e90
Lorena <lorena.tacal@gmail.com>
2020-01-08 11:29:21 +0000

    Create methos isLeapYear

---
+++ LeapYearCalculator/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import static com.company.LeapYear.isLeapYear;
+
+public class Main {
+
+    public static void main(String[] args) {
+      System.out.println(isLeapYear(1990));
+    }
+}
